7020abb706372875083075ecedcc190d013a1bfd
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2025-01-31 18:46:04 +0500

    changed

--- config/settings/base.py
+++ config/settings/base.py
@@ -12,9 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-@)h+#bed6dyiwfd)vkwlzv43g+ayi8y!nlyq$za&ts24+5$bik'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 LOCAL_APPS = [
