29c738f72e3d6d11c2c80cbf5417fe0dc75a418d
Codeco-Sobirjon <sobirbobojonov05763104@gmail.com>
2025-01-31 18:20:01 +0500

    change path media

--- config/settings/base.py
+++ config/settings/base.py
@@ -128,7 +128,7 @@ STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "/var/www/media/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
